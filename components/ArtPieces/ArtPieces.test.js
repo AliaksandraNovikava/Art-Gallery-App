fc15297016665a560897074a4ad8f0b6b043331d
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-06-23 14:55:56 +0200

    added Spotlight folder

--- components/ArtPieces/ArtPieces.test.js
+++ components/ArtPieces/ArtPieces.test.js
@@ -1,33 +1,30 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import ArtPieces from "../ArtPieces/index.js";
 
-const data = [{name: "Orange Red and Green Abstract Painting", artist: "Steve Johnson"},];
-
+const data = [
+  { name: "Orange Red and Green Abstract Painting", artist: "Steve Johnson" },
+];
 
 test("renders art pieces title", () => {
-  render(<ArtPieces pieces={data}/>);      
+  render(<ArtPieces pieces={data} />);
   const title = screen.getByText(/orange red and green abstract painting/i);
   expect(title).toBeInTheDocument();
-})
-
+});
 
 test("renders art pieces artist", () => {
-  render(<ArtPieces pieces={data}/>);
+  render(<ArtPieces pieces={data} />);
   const artist = screen.getByText(/steve johnson/i);
   expect(artist).toBeInTheDocument();
-})
-
+});
 
 test("renders image", () => {
-  render(<ArtPieces pieces={data}/>);
-  const image = screen.getByRole("img");     
+  render(<ArtPieces pieces={data} />);
+  const image = screen.getByRole("img");
   expect(image).toBeInTheDocument();
-})
-
+});
 
 test("renders list", () => {
-  render(<ArtPieces pieces={data}/>);
-  const list = screen.getByRole("list");     
+  render(<ArtPieces pieces={data} />);
+  const list = screen.getByRole("list");
   expect(list).toBeInTheDocument();
-})
+});
---
+++ components/Spotlight/index.js
@@ -0,0 +1,7 @@
+const {image, artist}
+
+export default function Spotlight (max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  

fd10e6da2a619304bfbb2e67072f5ebd6930bef0
Jivana Lipsewers <jivana.lipsewers@web.de>
2023-06-25 15:28:09 +0200

    Update UserStory3

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,31 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const fetcher = async (URL) => {
+  const res = await fetch(URL);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function App({ Component, pageProps }) {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  if (error) {
+    return <div>failed to load</div>;
+  }
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <>
       <GlobalStyle />
@@ -8,3 +33,5 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
+
+

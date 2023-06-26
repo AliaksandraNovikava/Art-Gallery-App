fc938e3bbe14e56f06efc1b07c453a299f8ca343
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-06-26 12:06:57 +0200

    add favoriteButton

--- components/FavoriteButton/index.js
+++ components/FavoriteButton/index.js
@@ -9,28 +9,3 @@ export default function FavoriteButton({ isFavorite, onToggleFavorite }) {
     ></button>
   );
 }
-
-/*
-export default function handle ({}) {
-
-} 
-
-function handleToggleFavorite({ id }) {
-      setIsFavorite(!isFavorite);
-    }
-
-    function handleToggleFavorite({id}) {
-      setEntries(entries.map(entry => 
-        { 
-          if(entry.id === id)
-          {
-            return ...entry, isFavorite: true};
-          }
-            else { return entry;}
-          }));
-
-           function toggleLiked() {
-    setLiked(!liked);
-  }
-  
-*/
